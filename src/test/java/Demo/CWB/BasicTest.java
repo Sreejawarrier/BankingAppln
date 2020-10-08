15029cb4ea803eecd31275a54c24d47e04a41852
sreeja warrier <sreejasreedeviwarrier@gmail.com>
2020-10-08 05:42:14 +0530

    BasicTest

---
+++ src/test/java/Demo/CWB/BasicTest.java
@@ -0,0 +1,51 @@
+package Demo.CWB;
+
+import org.testng.annotations.*;
+import pages.CbaLoginClass;
+
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class BasicTest extends TestBaseClass {
+	// WebDriver driver;
+    static CbaLoginClass lp;
+	public BasicTest() throws Exception {
+	super();
+	setUP();
+		}
+	
+ 	@Test
+	public void commSecTest() throws Exception {
+    lp=new CbaLoginClass(driver);
+	lp.testCommSecLogin();
+    assertTrue(driver.getCurrentUrl().endsWith("LoginFailed"));
+
+	}
+	@Test
+	public void netBankTest() throws Exception {
+	lp=new CbaLoginClass(driver);
+	lp.testNetbankLogin();
+    assertTrue(driver.getCurrentUrl().endsWith("CommBankVersion2"));
+
+	}
+		
+ 
+@AfterTest
+  public void afterTest() {
+	 driver.quit();
+	 assertTrue( true );
+  }
+
+}
+
